d6a9fe46e7350c7ea74f8bc99fb1f3caac42bb6f
Amanda Braga <amandabraga100@gmail.com>
2026-02-01 23:19:20 -0300

    Primeiro teste com codegen

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  
+  await page.getByTestId('search-order-id').click();
+  await page.getByTestId('search-order-id').fill('VLO-VTTHK5');
+  await page.getByTestId('search-order-button').click();
+
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-VTTHK5');
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
